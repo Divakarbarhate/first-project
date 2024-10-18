57cb9a1d8e72441e5b2efbb6d2fe5b9e4268a29c
Divakarbarhate <167970357+Divakarbarhate@users.noreply.github.com>
2024-10-18 19:12:32 +0530

    fix

--- components/CurdOperation/index.tsx
+++ components/CurdOperation/index.tsx
@@ -8,7 +8,7 @@ import { useState } from 'react';
 // const index
 const index = () => {
   const [users, setUsers] = useState([]);
-
+// heloo
   const addUser = (newUser) => {
     setUsers([...users, newUser]);
   };
